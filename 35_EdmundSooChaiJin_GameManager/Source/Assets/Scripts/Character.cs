53bb9d01dd23de5434e78b434d811801ae3e3321
Dygenlow <edmundsoo36@gmail.com>
2020-12-03 23:57:10 +0800

    Update 1.0

---
+++ 35_EdmundSooChaiJin_GameManager/Source/Assets/Scripts/Character.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    public float speed;
+
+    public int energyCount;
+
+    public GameObject energyCounter;
+    public Animator playerAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ForwardKey();
+        BackKey();
+        LeftKey();
+        RightKey();
+
+        energyCounter.GetComponent<Text>().text = "Energy: " + energyCount;
+
+        if(GameManager.instance.levelTime <= 0)
+        {
+            speed = 0;
+        }
+
+        if(energyCount < 0 || GameManager.instance.levelTime < 0)
+        {
+            GameManager.instance.LoseScene();
+        }
+
+        if(energyCount > 50)
+        {
+            GameManager.instance.WinScene();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("AddEnergy"))
+        {
+            energyCount = energyCount + 5;
+
+            GameManager.instance.AddTime();
+
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("MinusEnergy"))
+        {
+            energyCount = energyCount - 25;
+
+            Destroy(other.gameObject);
+        }
+    }
+    private void ForwardKey()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            playerAnim.SetBool("isMoving", true);
+        }
+
+        if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            playerAnim.SetBool("isMoving", false);
+        }
+    }
+
+    private void BackKey()
+    {
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+
+            playerAnim.SetBool("isMoving", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            playerAnim.SetBool("isMoving", false);
+        }
+    }
+
+    private void LeftKey()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+
+            playerAnim.SetBool("isMoving", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            playerAnim.SetBool("isMoving", false);
+        }
+    }
+
+    private void RightKey()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+
+            playerAnim.SetBool("isMoving", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            playerAnim.SetBool("isMoving", false);
+        }
+    }
+}
---
+++ 35_EdmundSooChaiJin_GameManager/Source/Assets/Scripts/GameManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameObject addEnergyPrefab;
+    public GameObject minusEnergyPrefab;
+    public GameObject timer;
+
+    public int numberOfSpawn;
+
+    public float levelTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        for(int i = 0; i < numberOfSpawn; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+
+            if(Random.Range(0, 2) < 1)
+            {
+                Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+            }
+
+            else
+            {
+                Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();  
+    }
+
+    public void AddTime()
+    {
+        levelTime = levelTime + 2;
+
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+
+            if (Random.Range(0, 2) < 1)
+            {
+                Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+            }
+
+            else
+            {
+                Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+            }
+        }
+    }
+
+    public string FormatTime(float time)
+    {
+        int minutes = (int)time / 60;
+        int seconds = (int)time - 60 * minutes;
+        int milliseconds = (int)(1000 * (time - minutes * 60 - seconds));
+        return string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+    }
+
+    public void Timer()
+    {
+        if (levelTime > 0)
+        {
+            levelTime -= Time.deltaTime;
+            timer.GetComponent<Text>().text = "Time Left: " + FormatTime(levelTime);
+        }
+
+        else
+        {
+            levelTime = 0;
+            timer.GetComponent<Text>().text = "          Times Up!";
+        }
+    }
+
+    public void LoseScene()
+    {
+        SceneManager.LoadScene("LoseScene");
+    }
+
+    public void WinScene()
+    {
+        SceneManager.LoadScene("WinScene");
+    }
+}
